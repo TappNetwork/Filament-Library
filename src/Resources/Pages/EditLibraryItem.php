efb34498500f724e0474c1eed4e4d8b34283ece6
scott grayson <Scott Grayson>
2025-09-15 13:12:02 -0400

    remove "move action"

--- src/Resources/Pages/EditLibraryItem.php
+++ src/Resources/Pages/EditLibraryItem.php
@@ -6,7 +6,6 @@ use Filament\Resources\Pages\EditRecord;
 use Tapp\FilamentLibrary\Resources\LibraryItemResource;
 use Filament\Actions\DeleteAction;
 use Filament\Actions\Action;
-use Filament\Forms\Components\Select;
 use Tapp\FilamentLibrary\Models\LibraryItem;
 
 class EditLibraryItem extends EditRecord
@@ -35,38 +34,6 @@ class EditLibraryItem extends EditRecord
                 );
         }
 
-        $actions[] = Action::make('move')
-                ->label('Move')
-                ->icon('heroicon-o-arrow-right-circle')
-                ->color('warning')
-                ->form([
-                    Select::make('parent_id')
-                        ->label('Move to folder')
-                        ->options(function () {
-                            $currentId = $this->getRecord()->id;
-
-                            return LibraryItem::where('type', 'folder')
-                                ->where('id', '!=', $currentId)
-                                ->where(function ($query) use ($currentId) {
-                                    // Prevent moving into self or descendants
-                                    $query->whereNull('parent_id')
-                                          ->orWhere('parent_id', '!=', $currentId);
-                                })
-                                ->pluck('name', 'id')
-                                ->prepend('Root (No parent)', null);
-                        })
-                        ->searchable()
-                        ->preload()
-                        ->default($this->getRecord()->parent_id),
-                ])
-                ->action(function (array $data): void {
-                    $this->getRecord()->update([
-                        'parent_id' => $data['parent_id'],
-                        'updated_by' => auth()->user()?->id,
-                    ]);
-
-                    $this->redirect(static::getResource()::getUrl('index', $data['parent_id'] ? ['parent' => $data['parent_id']] : []));
-                });
 
         $actions[] = DeleteAction::make();
 
