d0fce4833c789458b0020073c5837bb641fcd468
scott grayson <Scott Grayson>
2025-09-16 15:56:22 -0400

    fix: Implement proper responsive video embeds without negative margins
    - Replace negative margins with proper responsive CSS approach
    - Use video-container class with padding-bottom: 56.25% for 16:9 aspect ratio
    - Set iframe to position: absolute with width/height 100%
    - Remove all negative margin hacks and inline styles
    - Follow web standards for responsive video embeds
    - Video now fills available space naturally without breaking layout

--- src/Infolists/Components/VideoEmbed.php
+++ src/Infolists/Components/VideoEmbed.php
@@ -13,16 +13,16 @@ class VideoEmbed extends Entry
         if (str_contains($url, 'youtube.com') || str_contains($url, 'youtu.be')) {
             $videoId = $this->getYoutubeVideoId($url);
 
-            return '<div class="relative w-full" style="margin: -1.5rem -1.5rem; padding-bottom: 56.25%;"><iframe class="absolute top-0 left-0 w-full h-full rounded-lg" src="https://www.youtube.com/embed/' . $videoId . '" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>';
+            return '<div class="video-container"><iframe src="https://www.youtube.com/embed/' . $videoId . '" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>';
         } elseif (str_contains($url, 'vimeo.com')) {
             $videoId = $this->getVimeoVideoId($url);
             $hash = $this->getVimeoHash($url);
 
-            return '<div class="relative w-full" style="margin: -1.5rem -1.5rem; padding-bottom: 56.25%;"><iframe class="absolute top-0 left-0 w-full h-full rounded-lg" src="https://player.vimeo.com/video/' . $videoId . '?h=' . $hash . '&badge=0&autopause=0&player_id=0&app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media" allowfullscreen></iframe></div>';
+            return '<div class="video-container"><iframe src="https://player.vimeo.com/video/' . $videoId . '?h=' . $hash . '&badge=0&autopause=0&player_id=0&app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media" allowfullscreen></iframe></div>';
         } elseif (str_contains($url, 'wistia.com')) {
             $videoId = $this->getWistiaVideoId($url);
 
-            return '<div class="relative w-full" style="margin: -1.5rem -1.5rem; padding-bottom: 56.25%;"><iframe class="absolute top-0 left-0 w-full h-full rounded-lg" src="https://fast.wistia.net/embed/iframe/' . $videoId . '" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe></div>';
+            return '<div class="video-container"><iframe src="https://fast.wistia.net/embed/iframe/' . $videoId . '" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe></div>';
         }
 
         return '';
