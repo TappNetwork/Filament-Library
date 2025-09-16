9a596c197116dd7531117aba6534ca47ec4a1496
scott grayson <Scott Grayson>
2025-09-16 14:25:02 -0400

    feat: Add external links to library seeder for testing
    - Add Resources folder with 7 external links
    - Include 3 video links (YouTube, Vimeo, Wistia) for video embedding testing
    - Include 4 regular links (documentation, GitHub, Stack Overflow) for regular link testing
    - Add links in other folders (Project A, Graphics) to test mixed content
    - Each link includes custom icons and descriptions
    - Provides comprehensive test data for both video and non-video scenarios

--- database/seeders/LibrarySeeder.php
+++ database/seeders/LibrarySeeder.php
@@ -82,6 +82,13 @@ class LibrarySeeder extends Seeder
             'created_by' => $user->id,
         ]);
 
+        // Create a Resources folder for external links
+        $resources = LibraryItem::create([
+            'name' => 'Resources',
+            'type' => 'folder',
+            'created_by' => $user->id,
+        ]);
+
         // Create some sample files (without actual media attachments for now)
         $sampleFiles = [
             [
@@ -115,6 +122,88 @@ class LibrarySeeder extends Seeder
             ]);
         }
 
+        // Create sample external links - both video and non-video
+        $sampleLinks = [
+            // Video links (will be embedded)
+            [
+                'name' => 'Laravel Tutorial - YouTube',
+                'external_url' => 'https://www.youtube.com/watch?v=ImtZ5yENzgE',
+                'link_icon' => 'heroicon-o-play',
+                'link_description' => 'Complete Laravel tutorial for beginners',
+                'parent_id' => $resources->id,
+            ],
+            [
+                'name' => 'Filament Demo - Vimeo',
+                'external_url' => 'https://vimeo.com/123456789',
+                'link_icon' => 'heroicon-o-video-camera',
+                'link_description' => 'Filament admin panel demonstration',
+                'parent_id' => $resources->id,
+            ],
+            [
+                'name' => 'Wistia Video Example',
+                'external_url' => 'https://tappnetwork.wistia.com/medias/abc123def',
+                'link_icon' => 'heroicon-o-film',
+                'link_description' => 'Example Wistia video for testing',
+                'parent_id' => $resources->id,
+            ],
+            // Non-video links (will open in new tab)
+            [
+                'name' => 'Laravel Documentation',
+                'external_url' => 'https://laravel.com/docs',
+                'link_icon' => 'heroicon-o-book-open',
+                'link_description' => 'Official Laravel documentation',
+                'parent_id' => $resources->id,
+            ],
+            [
+                'name' => 'Filament Documentation',
+                'external_url' => 'https://filamentphp.com/docs',
+                'link_icon' => 'heroicon-o-academic-cap',
+                'link_description' => 'Filament admin panel documentation',
+                'parent_id' => $resources->id,
+            ],
+            [
+                'name' => 'GitHub Repository',
+                'external_url' => 'https://github.com/TappNetwork/Filament-Library',
+                'link_icon' => 'heroicon-o-code-bracket',
+                'link_description' => 'Source code for this library plugin',
+                'parent_id' => $resources->id,
+            ],
+            [
+                'name' => 'Stack Overflow',
+                'external_url' => 'https://stackoverflow.com/questions/tagged/laravel',
+                'link_icon' => 'heroicon-o-question-mark-circle',
+                'link_description' => 'Laravel questions and answers',
+                'parent_id' => $resources->id,
+            ],
+            // Links in other folders
+            [
+                'name' => 'Project Reference',
+                'external_url' => 'https://example.com/project-reference',
+                'link_icon' => 'heroicon-o-link',
+                'link_description' => 'Reference documentation for Project A',
+                'parent_id' => $projectA->id,
+            ],
+            [
+                'name' => 'Design Inspiration',
+                'external_url' => 'https://dribbble.com/shots/example',
+                'link_icon' => 'heroicon-o-paint-brush',
+                'link_description' => 'Design inspiration for graphics',
+                'parent_id' => $graphics->id,
+            ],
+        ];
+
+        foreach ($sampleLinks as $linkData) {
+            LibraryItem::create([
+                'name' => $linkData['name'],
+                'type' => 'link',
+                'external_url' => $linkData['external_url'],
+                'link_icon' => $linkData['link_icon'],
+                'link_description' => $linkData['link_description'],
+                'parent_id' => $linkData['parent_id'],
+                'created_by' => $user->id,
+            ]);
+        }
+
         // Create some sample permissions if there are other users
         $otherUsers = \App\Models\User::where('id', '!=', $user->id)->take(2)->get();
 
@@ -144,13 +233,16 @@ class LibrarySeeder extends Seeder
         $this->command->info('Created:');
         $this->command->info('- Documents/');
         $this->command->info('  - Projects/');
-        $this->command->info('    - Project A/ (with 2 files)');
+        $this->command->info('    - Project A/ (with 2 files + 1 link)');
         $this->command->info('    - Project B/ (with 1 file)');
         $this->command->info('  - Templates/ (with 1 file)');
         $this->command->info('  - Meeting Notes.txt');
         $this->command->info('- Images/');
         $this->command->info('  - Photos/');
-        $this->command->info('  - Graphics/');
+        $this->command->info('  - Graphics/ (with 1 link)');
+        $this->command->info('- Resources/ (with 7 external links)');
+        $this->command->info('  - 3 video links (YouTube, Vimeo, Wistia)');
+        $this->command->info('  - 4 regular links (documentation, GitHub, etc.)');
 
         if ($otherUsers->count() > 0) {
             $this->command->info('Sample permissions created for other users.');
